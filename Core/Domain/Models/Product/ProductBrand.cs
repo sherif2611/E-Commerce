7735a376d31ea72729a4cead54448f060757c658
Sherif Ebrahim <90417294+sherif2611@users.noreply.github.com>
2025-04-17 17:24:58 +0200

    AddBaseEntityAndProductEntities

---
+++ Core/Domain/Models/Product/ProductBrand.cs
@@ -0,0 +1,7 @@
+﻿namespace Domain.Models.Product
+{
+	public class ProductBrand:BaseEntity<int>
+	{
+        public string Name { get; set; } = default!;
+    }
+}
---
+++ Core/Domain/Models/Product/ProductEntity.cs
@@ -0,0 +1,10 @@
+﻿namespace Domain.Models.Product
+{
+	public class ProductEntity:BaseEntity<int>
+	{
+		public string Name { get; set; } = default!;
+		public string Description { get; set; } = default!;
+		public string PictureUrl { get; set; } = default!;
+		public decimal Price { get; set; }
+    }
+}
