5b41335c695e55d8ed011bebf73d4fdc413fd35f
Sherif Ebrahim <90417294+sherif2611@users.noreply.github.com>
2025-04-18 14:45:08 +0200

    CreateGenericRepository

--- Infrastructure/Persistence/GlobalUsing.cs
+++ Infrastructure/Persistence/GlobalUsing.cs
@@ -4,3 +4,4 @@ global using Microsoft.EntityFrameworkCore.Metadata.Builders;
 global using Domain.Contracts;
 global using Persistence.Data;
 global using System.Text.Json;
+global using Domain.Models;
